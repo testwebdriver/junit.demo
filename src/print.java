6779d4ee0ac01eb360d2162c98a6b6c994aba1f8
ammu <ponaruna@gmail.com>
2013-07-19 09:38:12 -0700

    More quotes added

--- src/print.java
+++ src/print.java
@@ -4,12 +4,21 @@ import org.junit.Test;
 public class print {
 	
 	@Test
-	public void printquote(){
+	public void printquote_Gandhi(){
 		
 		
 		System.out.println("truth alone triumphs");
 		System.out.println("Where there is love there is life.");
 		System.out.println("An eye for an eye only ends up making the whole world blind");
 	}
+	
+	@Test
+	public void printquote_MKing(){
+		
+		System.out.println("Darkness cannot drive out darkness; only light can do that. Hate cannot drive out hate; only love can do that");
+		
 
+	}
+		
+	
 }
